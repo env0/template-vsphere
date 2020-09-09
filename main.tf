81e4c803c81d707a1310bf56f8672b3dd122e071
Roni Frantchi <roni-frantchi@users.noreply.github.com>
2020-09-09 10:21:39 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,11 @@ resource "vsphere_virtual_machine" "vm" {
   resource_pool_id = data.vsphere_resource_pool.pool.id
   datastore_id     = data.vsphere_datastore.datastore.id
 
+  ovf_deploy {
+    // Url to remote ovf/ova file
+    remote_ovf_url = "https://download3.vmware.com/software/vmw-tools/nested-esxi/Nested_ESXi7.0_Appliance_Template_v1.ova"
+  }
+  
   num_cpus = 2
   memory   = 1024
   guest_id = "other3xLinux64Guest"
