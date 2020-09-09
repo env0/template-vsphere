dc9820349c3a16bebc25ce1bdb653d7aab1b5312
Roni Frantchi <roni-frantchi@users.noreply.github.com>
2020-09-09 10:22:47 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,25 +3,6 @@ provider "vsphere" {
   allow_unverified_ssl = true
 }
 
-data "vsphere_datacenter" "dc" {
-  name = "dc1"
-}
-
-data "vsphere_datastore" "datastore" {
-  name          = "datastore1"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_resource_pool" "pool" {
-  name          = "cluster1/Resources"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_network" "network" {
-  name          = "public"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
 resource "vsphere_virtual_machine" "vm" {
   name             = "terraform-test"
   resource_pool_id = data.vsphere_resource_pool.pool.id
@@ -44,4 +25,31 @@ resource "vsphere_virtual_machine" "vm" {
     label = "disk0"
     size  = 20
   }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+data "vsphere_datacenter" "dc" {
+  name = "dc1"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "datastore1"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = "cluster1/Resources"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = "public"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
 }
